44795aa9134cbb737d227a5e40f4fc345ee9e5fe
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-02-08 17:22:39 +0530

    Added Basic Authentication

--- socialsearch/views.py
+++ socialsearch/views.py
@@ -1,35 +1,22 @@
 from pyramid.response import Response
-from pyramid.view import view_config
+from pyramid.view import view_config,forbidden_view_config
+from pyramid.security import remember,authenticated_userid, forget, Authenticated
+from pyramid.httpexceptions import HTTPFound
 
-from sqlalchemy.exc import DBAPIError
+from models import DBSession
 
-from .models import (
-    DBSession,
-    MyModel,
-    )
+from sqlalchemy import and_
+import hashlib
 
+@view_config(route_name='home',renderer='json', permission='__no_permission_required__')
+def homeView(request):
+    return {'hi' : 'user'}
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'SocialSearch'}
+@forbidden_view_config()
+def forbidden(request):
+    return Response('Not Allowed')
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_SocialSearch_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
+@view_config(context='pyramid.exceptions.NotFound', renderer='json', permission='__no_permission_required__')
+def notFound_view(request):
+    notFound404 = '404 Not Found'
+    return {'error' : notFound404}
